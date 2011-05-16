0f068f2ef279408a079a68ebcd70b68f7ba1556d
Andrew Nesbitt <andrewnez@gmail.com>
2011-05-16 11:10:41 -0400

    Stolen resque layout and styles for frontend

--- lib/multivariate/server.rb
+++ lib/multivariate/server.rb
@@ -5,11 +5,24 @@ module Multivariate
   class Server < Sinatra::Base
     enable :sessions
     dir = File.dirname(File.expand_path(__FILE__))
+
     set :views,  "#{dir}/server/views"
+    set :public, "#{dir}/server/public"
+    set :static, true
+
+    helpers do
+      def url(*path_parts)
+        [ path_prefix, path_parts ].join("/").squeeze('/')
+      end
+
+      def path_prefix
+        request.env['SCRIPT_NAME']
+      end
+    end
 
     get '/' do
       @experiments = Multivariate::Experiment.all
-      render 'hello'
+      erb :index
     end
   end
 end
