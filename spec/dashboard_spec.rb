eae106cb96c12865c902fff4d188d827a23f4e33
Andrew Nesbitt <andrewnez@gmail.com>
2011-06-10 16:42:05 +0100

    added dashboard reset test

--- spec/dashboard_spec.rb
+++ spec/dashboard_spec.rb
@@ -13,4 +13,26 @@ describe Split::Dashboard do
     get '/'
     last_response.should be_ok
   end
+
+  it "should reset an experiment" do
+    experiment = Split::Experiment.find_or_create('link_color', 'blue', 'red')
+    red = Split::Alternative.find('red', 'link_color').participant_count
+
+    red = Split::Alternative.find('red', 'link_color')
+    blue = Split::Alternative.find('blue', 'link_color')
+    red.participant_count = 5
+    red.save
+    blue.participant_count = 6
+    blue.save
+
+    post '/reset/link_color'
+
+    last_response.should be_redirect
+
+    new_red_count = Split::Alternative.find('red', 'link_color').participant_count
+    new_blue_count = Split::Alternative.find('blue', 'link_color').participant_count
+
+    new_blue_count.should eql(0)
+    new_red_count.should eql(0)
+  end
 end
