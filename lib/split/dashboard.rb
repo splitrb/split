614434d8929169d2237e65aae7219af00a148922
Andrew Nesbitt <andrewnez@gmail.com>
2011-06-26 13:18:44 +0100

    Show confidence level instead of zscore in dashboard

--- lib/split/dashboard.rb
+++ lib/split/dashboard.rb
@@ -27,6 +27,33 @@ module Split
       def round(number, precision = 2)
         BigDecimal.new(number.to_s).round(precision).to_f
       end
+
+      def confidence_level(z_score)
+        z = z_score.to_f
+        if z > 0.0
+          if z < 1.96
+            'no confidence'
+          elsif z < 2.57
+            '95% confidence'
+          elsif z < 3.29
+            '99% confidence'
+          else
+            '99.9% confidence'
+          end
+        elsif z < 0.0
+          if z > -1.96
+            'no confidence'
+          elsif z > -2.57
+            '95% confidence'
+          elsif z > -3.29
+            '99% confidence'
+          else
+            '99.9% confidence'
+          end
+        else
+          "No Change"
+        end
+      end
     end
 
     get '/' do
