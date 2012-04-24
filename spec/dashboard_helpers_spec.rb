02b59b0fbbab15718890a780c649f63f02faf5a7
Elliot Crosby-McCullough <elliot.cm@gmail.com>
2012-04-24 09:21:21 +0100

    Add confidence helper tests.
    To cover a refactor.

--- spec/dashboard_helpers_spec.rb
+++ spec/dashboard_helpers_spec.rb
@@ -8,5 +8,13 @@ describe Split::DashboardHelpers do
     it 'should handle very small numbers' do
       confidence_level(Complex(2e-18, -0.03)).should eql('No Change')
     end
+
+    it "should consider a z-score of 1.96 < z < 2.57 as 95% confident" do
+      confidence_level(2.12).should eql('95% confidence')
+    end
+
+    it "should consider a z-score of -1.96 > z > -2.57 as 95% confident" do
+      confidence_level(-2.12).should eql('95% confidence')
+    end
   end
 end
