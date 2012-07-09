92550a696f09df6319a69f7d17ded7727d2bcabb
Brandon Carpenter <bhcarpenter@gmail.com>
2012-07-09 14:46:26 -0400

    Allow parameter overrides, even without Redis.
    With this option, applications can be tested to ensure that the options
    render correctly, even without Redis installed and running on the
    development machines.

--- lib/split/configuration.rb
+++ lib/split/configuration.rb
@@ -4,6 +4,7 @@ module Split
     attr_accessor :ignore_ip_addresses
     attr_accessor :db_failover
     attr_accessor :db_failover_on_db_error
+    attr_accessor :db_failover_allow_parameter_override
     attr_accessor :allow_multiple_experiments
     attr_accessor :enabled
 
@@ -12,6 +13,7 @@ module Split
       @ignore_ip_addresses = []
       @db_failover = false
       @db_failover_on_db_error = proc{|error|} # e.g. use Rails logger here
+      @db_failover_allow_parameter_override = false
       @allow_multiple_experiments = false
       @enabled = true
     end
