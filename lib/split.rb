eaa0ec736a55737b5eb188534380b8bf5e441e56
buddhamagnet <buddhamagnet@gmail.com>
2012-11-09 15:03:29 +0000

    modified split main file for backend

--- lib/split.rb
+++ lib/split.rb
@@ -9,41 +9,6 @@ module Split
   extend self
   attr_accessor :configuration
 
-  # Accepts:
-  #   1. A 'hostname:port' string
-  #   2. A 'hostname:port:db' string (to select the Redis db)
-  #   3. A 'hostname:port/namespace' string (to set the Redis namespace)
-  #   4. A redis URL string 'redis://host:port'
-  #   5. An instance of `Redis`, `Redis::Client`, `Redis::DistRedis`,
-  #      or `Redis::Namespace`.
-  def redis=(server)
-    if server.respond_to? :split
-      if server["redis://"]
-        redis = Redis.connect(:url => server, :thread_safe => true)
-      else
-        server, namespace = server.split('/', 2)
-        host, port, db = server.split(':')
-        redis = Redis.new(:host => host, :port => port,
-          :thread_safe => true, :db => db)
-      end
-      namespace ||= :split
-
-      @redis = Redis::Namespace.new(namespace, :redis => redis)
-    elsif server.respond_to? :namespace=
-        @redis = server
-    else
-      @redis = Redis::Namespace.new(:split, :redis => server)
-    end
-  end
-
-  # Returns the current Redis connection. If none has been created, will
-  # create a new one.
-  def redis
-    return @redis if @redis
-    self.redis = 'localhost:6379'
-    self.redis
-  end
-
   # Call this method to modify defaults in your initializers.
   #
   # @example
@@ -53,6 +18,10 @@ module Split
   def configure
      self.configuration ||= Configuration.new
      yield(configuration)
+   end
+   
+   def db
+     @db ||= Split::Database.new
    end
 end
 
