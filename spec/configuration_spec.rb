ab5abff9ec562bf4239df5c0679ef7f201c5dac1
buddhamagnet <buddhamagnet@gmail.com>
2012-11-11 19:47:10 +0000

    split config specs into multiple tests

--- spec/configuration_spec.rb
+++ spec/configuration_spec.rb
@@ -4,12 +4,26 @@ describe Split::Configuration do
   
   before(:each) { @config = Split::Configuration.new }
   
-  it "should provide default values" do
+  it "should provide a default value for ignore_ip_addresses" do
     @config.ignore_ip_addresses.should eql([])
-    @config.robot_regex.should eql(/\b(Baidu|Gigabot|Googlebot|libwww-perl|lwp-trivial|msnbot|SiteUptime|Slurp|WordPress|ZIBB|ZyBorg)\b/i)
+  end
+  
+  it "should provide default values for db failover" do
     @config.db_failover.should be_false
-    @config.db_failover_on_db_error.should be_a Proc
+    @config.db_failover_on_db_error.should be_a Proc  
+  end
+  
+  it "should not allow multiple experiments by default" do
     @config.allow_multiple_experiments.should be_false
+  end
+  
+  it "should be enabled by default" do
     @config.enabled.should be_true
+  end
+  
+  it "should provide a default pattern for robots" do
+    %w[Baidu Gigabot Googlebot libwww-perl lwp-trivial msnbot SiteUptime Slurp WordPress ZIBB ZyBorg].each do |robot|
+      @config.robot_regex.should =~ robot
+    end
   end
 end
