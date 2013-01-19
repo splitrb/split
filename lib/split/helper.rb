7ed461f40f4a464c5d17b8daf2980428bb066837
Ian Young <ian.greenleaf@gmail.com>
2013-01-18 16:39:38 -0800

    Don't spit out 1.8 errors for preloaded config
    Regression in #105.

--- lib/split/helper.rb
+++ lib/split/helper.rb
@@ -2,7 +2,7 @@ module Split
   module Helper
 
     def ab_test(experiment_name, control=nil, *alternatives)
-      if RUBY_VERSION.match(/1\.8/) && alternatives.length.zero?
+      if RUBY_VERSION.match(/1\.8/) && alternatives.length.zero? && ! control.nil?
         puts 'WARNING: You should always pass the control alternative through as the second argument with any other alternatives as the third because the order of the hash is not preserved in ruby 1.8'
       end
 
