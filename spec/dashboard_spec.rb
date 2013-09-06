d1b2ae2c67959076dec18e0f5ceea041a3eccd18
Hugo Duksis <hugo.duksis@gmail.com>
2013-09-06 17:36:09 +0200

    Renaming link_color to experiment

--- spec/dashboard_spec.rb
+++ spec/dashboard_spec.rb
@@ -5,39 +5,33 @@ require 'split/dashboard'
 describe Split::Dashboard do
   include Rack::Test::Methods
 
-  let(:link_color) {
-    Split::Experiment.find_or_create('link_color', 'blue', 'red')
-  }
-
-  def link(color)
-    Split::Alternative.new(color, 'link_color')
-  end
-
-  let(:red_link) {
-    link("red")
-  }
-
-  let(:blue_link) {
-    link("blue")
-  }
-
   def app
     @app ||= Split::Dashboard
   end
+
+  def link(color)
+    Split::Alternative.new(color, experiment.name)
+  end
+
+  let(:experiment) {
+    Split::Experiment.find_or_create('link_color', 'blue', 'red')
+  }
+
+  let(:red_link) { link("red") }
+  let(:blue_link) { link("blue") }
 
   it "should respond to /" do
     get '/'
     last_response.should be_ok
   end
 
-  it "should reset an experiment" do
-    experiment = link_color
 
+  it "should reset an experiment" do
     red_link.participant_count = 5
     blue_link.participant_count = 7
     experiment.winner = 'blue'
 
-    post '/reset/link_color'
+    post "/reset/#{experiment.name}"
 
     last_response.should be_redirect
 
@@ -50,17 +44,14 @@ describe Split::Dashboard do
   end
 
   it "should delete an experiment" do
-    experiment = link_color
-    delete '/link_color'
+    delete "/#{experiment.name}"
     last_response.should be_redirect
-    Split::Experiment.find('link_color').should be_nil
+    Split::Experiment.find(experiment.name).should be_nil
   end
 
   it "should mark an alternative as the winner" do
-    experiment = link_color
     experiment.winner.should be_nil
-
-    post '/link_color', :alternative => 'red'
+    post "/#{experiment.name}", :alternative => 'red'
 
     last_response.should be_redirect
     experiment.winner.name.should eql('red')
@@ -69,7 +60,7 @@ describe Split::Dashboard do
   it "should display the start date" do
     experiment_start_time = Time.parse('2011-07-07')
     Time.stub(:now => experiment_start_time)
-    experiment = link_color
+    experiment
 
     get '/'
 
@@ -79,8 +70,6 @@ describe Split::Dashboard do
   it "should handle experiments without a start date" do
     experiment_start_time = Time.parse('2011-07-07')
     Time.stub(:now => experiment_start_time)
-    experiment = link_color
-
     Split.redis.hdel(:experiment_start_times, experiment.name)
 
     get '/'
