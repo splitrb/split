217d41d20c654a8d02746e5b67fcee97de497c80
Jonas Huckestein <jonas.huckestein@gmail.com>
2013-10-02 17:15:27 -0400

    Add Split::EncapsulatedHelper that you can mix into Rails controllers and models that will only expose ab_test and ab_test_finished

---
+++ lib/split/encapsulated_helper.rb
@@ -0,0 +1,52 @@
+# Split's helper exposes all kinds of methods we don't want to
+# mix into our model classes.
+#
+# This module exposes only two methods
+#  - ab_test and 
+#  - ab_test_finished
+# that can safely be mixed into any class.
+#
+# Passes the instance of the class that it's mixed into to the
+# Split persistence adapter as context.
+#
+module Split
+  module EncapsulatedHelper
+
+    class ContextShim
+      include Split::Helper
+      def initialize(context)
+        @context = context
+      end
+      def ab_user
+        @ab_user ||= Split::Persistence.adapter.new(@context)
+      end
+    end
+
+    def ab_test(*arguments)
+      ret = split_context_shim.ab_test(*arguments)
+      # TODO there must be a better way to pass a block straight
+      # through to the original ab_test
+      if block_given?
+        if defined?(capture) # a block in a rails view
+          block = Proc.new { yield(ret) }
+          concat(capture(ret, &block))
+          false
+        else
+          yield(ret)
+        end
+      else
+        ret
+      end
+    end
+
+    def ab_test_finished(*arguments)
+      split_context_shim.finished *arguments
+    end
+
+    private
+
+    def split_context_shim
+      @split_context_shim ||= ContextShim.new self
+    end
+  end
+end
