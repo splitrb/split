aea3c5f7675810624e49293f416ed65a98ead52c
Andrei Bondarev <andrei.bondarev13@gmail.com>
2016-05-03 18:47:06 -0400

    Updated spec file

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -4,9 +4,9 @@ require 'split/experiment'
 require 'split/user'
 
 describe Split::User do
-  let(:context) do
-    double(:session => { split: { 'link_color' => 'blue' } })
-  end
+  let(:user_keys) { { 'link_color' => 'blue' } }
+  let(:context) { double(:session => { split:  user_keys }) }
+  let(:experiment) { Split::Experiment.new('link_color') }
 
   before(:each) do
     @subject = described_class.new(context)
@@ -16,11 +16,18 @@ describe Split::User do
     expect(@subject['link_color']).to eq(@subject.user['link_color'])
   end
 
-  context '#cleanup_old_experiments' do
-    let(:experiment) { Split::Experiment.new('link_color') }
+  context '#cleanup_old_versions!' do
+    let(:user_keys) { { 'link_color:1' => 'blue' } }
 
+    it 'removes key if old experiment is found' do
+      @subject.cleanup_old_versions!(experiment)
+      expect(@subject.keys).to be_empty
+    end
+  end 
+
+  context '#cleanup_old_experiments!' do
     it 'removes key if experiment is not found' do
-      @subject.cleanup_old_experiments
+      @subject.cleanup_old_experiments!
       expect(@subject.keys).to be_empty
     end
 
@@ -28,14 +35,14 @@ describe Split::User do
       allow(Split::ExperimentCatalog).to receive(:find).with('link_color').and_return(experiment)
       allow(experiment).to receive(:start_time).and_return(Date.today)
       allow(experiment).to receive(:has_winner?).and_return(true)
-      @subject.cleanup_old_experiments
+      @subject.cleanup_old_experiments!
       expect(@subject.keys).to be_empty
     end
 
     it 'removes key if experiment has not started yet' do
       allow(Split::ExperimentCatalog).to receive(:find).with('link_color').and_return(experiment)
       allow(experiment).to receive(:has_winner?).and_return(false)
-      @subject.cleanup_old_experiments
+      @subject.cleanup_old_experiments!
       expect(@subject.keys).to be_empty
     end    
   end
