9785d2637f89c398af969ff50a3e7df9a36043ab
Brad Hudson <bradley.hudson@careerbuilder.com>
2016-10-03 14:06:59 -0400

    assign the instance variable

--- lib/split/alternative.rb
+++ lib/split/alternative.rb
@@ -33,7 +33,7 @@ module Split
 
     def p_winner(goal = nil)
       field = set_prob_field(goal)
-      Split.redis.hget(key, field).to_f
+      @p_winner = Split.redis.hget(key, field).to_f
     end
 
     def set_p_winner(prob, goal = nil)
