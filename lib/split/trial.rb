1bc6797ac7e8e7e4b0ee5f1c6fcbf1d925a541f9
Steven Ou <stevenou@gmail.com>
2017-02-05 23:05:29 -0800

    trial executes unique and non-unique completion increments + finishing and participating in the experiments

--- lib/split/trial.rb
+++ lib/split/trial.rb
@@ -7,7 +7,6 @@ module Split
     
     def initialize(attrs = {})
       self.experiment = attrs[:experiment]  if !attrs[:experiment].nil?
-      self.alternative = attrs[:alternative] if !attrs[:alternative].nil?
       self.goals = attrs[:goals].nil? ? [] : attrs[:goals]
       self.value = attrs[:value] if !attrs[:value].nil?
       self.user = attrs[:user] if !attrs[:user].nil?
@@ -16,30 +15,45 @@ module Split
     def alternative
       @alternative ||=  if experiment.has_winner?
                           experiment.winner
+                        else
+                          choose
                         end
     end
 
     def complete!
       if alternative
         if self.goals.empty?
+          if !experiment.finished?(split_id)
+            alternative.increment_unique_completion
+            experiment.finish!(split_id)
+          end
           alternative.increment_completion
         else
-          self.goals.each {|g| alternative.increment_completion(g, self.value)}
+          self.goals.each {|g|
+            if !experiment.finished?(split_id, g)
+              alternative.increment_unique_completion(g, self.value)
+              experiment.finish!(split_id, g)
+            end
+            alternative.increment_completion(g, self.value)
+          }
         end
       end
     end
 
     def choose!
       choose
-      record!
+      if !experiment.participating?(split_id)
+        record!
+      end
     end
 
     def record!
       alternative.increment_participation
+      experiment.participate!(split_id)
     end
 
     def choose
-      self.alternative = experiment.next_alternative
+      self.alternative = experiment.next_alternative(split_id)
     end
 
     def alternative=(alternative)
