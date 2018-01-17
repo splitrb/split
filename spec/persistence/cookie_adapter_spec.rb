d80055d512f92d67f5c764fd56f51760303aa8fd
Paul <paul@thoughtless.ca>
2018-01-16 22:22:23 -0800

    Add failing spec for multiple Set-Cookie headers bug

--- spec/persistence/cookie_adapter_spec.rb
+++ spec/persistence/cookie_adapter_spec.rb
@@ -40,4 +40,9 @@ describe Split::Persistence::CookieAdapter do
     expect(subject["my_key"]).to eq("my_value")
   end
 
+  it "puts multiple experiments in a single cookie" do
+    subject["foo"] = "FOO"
+    subject["bar"] = "BAR"
+    expect(context.response.headers["Set-Cookie"]).to match(/\Asplit=%7B%22foo%22%3A%22FOO%22%2C%22bar%22%3A%22BAR%22%7D; path=\/; expires=[a-zA-Z]{3}, \d{2} [a-zA-Z]{3} \d{4} \d{2}:\d{2}:\d{2} -0000\Z/)
+  end
 end
