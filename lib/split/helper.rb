27cf04c0bed70ab92824923137209bf098170dbb
André Luis Leal Cardoso Junior <andrehjr@gmail.com>
2019-02-25 22:38:50 -0300

    Checks for defined?(request) on Helper#exclude_visitor?

--- lib/split/helper.rb
+++ lib/split/helper.rb
@@ -122,7 +122,7 @@ module Split
     end
 
     def exclude_visitor?
-      instance_exec(request, &Split.configuration.ignore_filter) || is_ignored_ip_address? || is_robot? || is_preview?
+      defined?(request) && (instance_exec(request, &Split.configuration.ignore_filter) || is_ignored_ip_address? || is_robot? || is_preview?)
     end
 
     def is_robot?
