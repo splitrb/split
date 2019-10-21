7834881811249cab180165d4b8d70df788f25dc8
André Luis Leal Cardoso Junior <andrehjr@gmail.com>
2019-10-21 13:02:48 -0300

    Fix unused variable warnings on specs

--- spec/experiment_spec.rb
+++ spec/experiment_spec.rb
@@ -460,7 +460,7 @@ describe Split::Experiment do
       end
 
       it "should reset an experiment if it is loaded with different goals" do
-        same_experiment = same_but_different_goals
+        same_but_different_goals
         expect(Split::ExperimentCatalog.find("link_color").goals).to eq(["purchase", "refund"])
       end
 
