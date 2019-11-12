9d9797dc08e74d033fd783bb7c09692c1156df90
André Luis Leal Cardoso Junior <andrehjr@gmail.com>
2019-11-12 12:53:48 -0300

    Reference ActionController directly when including split helpers (#602)

--- lib/split/engine.rb
+++ lib/split/engine.rb
@@ -4,10 +4,10 @@ module Split
     initializer "split" do |app|
       if Split.configuration.include_rails_helper
         ActiveSupport.on_load(:action_controller) do
-          include Split::Helper
-          helper Split::Helper
-          include Split::CombinedExperimentsHelper
-          helper Split::CombinedExperimentsHelper
+          ::ActionController::Base.send :include, Split::Helper
+          ::ActionController::Base.helper Split::Helper
+          ::ActionController::Base.send :include, Split::CombinedExperimentsHelper
+          ::ActionController::Base.helper Split::CombinedExperimentsHelper
         end
       end
     end
