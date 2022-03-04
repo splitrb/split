f723890085b633f009045ed61884599a3bd74ead
André Luis Leal Cardoso Junior <andrehjr@gmail.com>
2022-03-04 19:32:25 -0300

    Fix Style/DefWithParentheses

--- lib/split/helper.rb
+++ lib/split/helper.rb
@@ -105,7 +105,7 @@ module Split
       Split.configuration.db_failover_on_db_error.call(e)
     end
 
-    def ab_active_experiments()
+    def ab_active_experiments
       ab_user.active_experiments
     rescue => e
       raise unless Split.configuration.db_failover
